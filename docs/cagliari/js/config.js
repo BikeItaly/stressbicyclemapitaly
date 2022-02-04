5961b53a12c2990997c97068926f2705a0350678
napo <napo@fbk.eu>
2022-02-04 16:20:45 +0100

    added Cagliari

---
+++ docs/cagliari/js/config.js
@@ -0,0 +1,6 @@
+var MAPBOX_TOKEN = 'pk.eyJ1IjoibmFwbyIsImEiOiJ5dG5HeDFZIn0.v4diQq5k3tVzeyRdjYks_A';
+var MAPBOX_STYLE = 'mapbox://styles/mapbox/streets-v11';
+var STRESSCYCLELEVES_TILESET = 'mapbox://napo.stress_cycle_levels'
+var STRESSCYCLELEVES_SOURCE = 'stress_cycle_levels'
+var STRESSCYCLELEVES_LAYER = 'stresslevels'
+const LEVELSANDCOLORS = ['level_1', '#0099cc', 'level_2', '#1C7C54','level_3','#F0C808','level_4','#DD5454'];
